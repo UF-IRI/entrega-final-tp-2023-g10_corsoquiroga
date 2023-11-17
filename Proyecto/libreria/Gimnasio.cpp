ed795a363dfdacf0873ce81ce13c0a8d58a786e1
Luana Quiroga <102482603+LuanaQuiroga@users.noreply.github.com>
2023-11-16 22:37:58 -0300

    cambios en gimnasio y otros

--- Proyecto/libreria/Gimnasio.cpp
+++ Proyecto/libreria/Gimnasio.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -30,21 +32,99 @@ int determinarHora() {
 }
 
 
-int anotarAclase(const string& clase, sClase *clase, sCliente*cliente){
+int anotarAclase(const string& clase, sClase *clase, sCliente *cliente){
+
     switch (clase){
     case "Spinning":
+        if(clase->tipo == clase && clase->cupoMax<45){
+
+            clase->cupoMax++;
+        }
     case "Yoga":
+        if(clase->tipo == clase && clase->cupoMax<25){
+            clase->cupoMax++;
+        }
     case "Pilates":
+        if(clase->tipo == clase && clase->cupoMax<15){
+            clase->cupoMax++;
+        }
     case "Stretching":
+        if(clase->tipo == clase && clase->cupoMax<40){
+            clase->cupoMax++;
+        }
     case  "Zumba":
+        if(clase->tipo == clase && clase->cupoMax<50){
+            clase->cupoMax++;
+        }
     case "Boxeo":
+        if(clase->tipo == clase && clase->cupoMax<30){
+            clase->cupoMax++;
+        }
     case "Musculacion":
+        if(clase->tipo == clase && clase->cupoMax<30){
+            clase->cupoMax++;
+        }
     default: return -1;
     }
     return 0;
 }
 
-eCodArchivos leerArchivoClase(std::fstream	*archi, sClase *clase){}
-eCodArchivos escribirArchivoClase(std::fstream	*archi, sClase *clase){}
-eCodArchivos leerArchivoCliente(std::fstream *archi, sCliente *cliente){}
-eCodArchivos escribirArchivoCliente(std::fstream	*archi, sCliente *cliente){}
+//ARCHIVOS
+eCodArchivos leerArchivoClase(std::fstream	*archi, sClase *clase){
+
+    if(!archi->is_open())
+        return eCodArchivos::ErrorApertura;
+
+    archi->read((char*)clase, sizeof(sClase));
+
+    if(!*archi){
+        return eCodArchivos::ErrorEscritura;
+    }
+
+    return eCodArchivos::ExitoOperacion;
+}
+
+eCodArchivos escribirArchivoClase(std::fstream	*archi, sClase *clase){
+    if(!archi->is_open())
+        return eCodArchivos::ErrorApertura;
+
+    archi->write((char*)clase, sizeof(sClase));
+
+    return eCodArchivos::ExitoOperacion;
+}
+
+eCodArchivos leerArchivoCliente(std::fstream *archi, sCliente *cliente){
+
+    unsigned int auxId;
+    string auxNombre, auxApellido, auxEmail, auxTelefono;
+    float auxEstado;
+    time_t auxFecha;
+    char coma;
+    int i=0;
+
+    if(!archi->is_open())
+        return eCodArchivos::ErrorApertura;
+
+    while(*archi>>auxId>>coma>>auxNombre>>coma>>auxApellido>>coma>>auxEmail>>coma>>auxTelefono>>coma>>auxFecha>>coma>>auxEstado){
+        (cliente+i)->ID_Cliente=auxId;
+        (cliente+i)->nombre=auxNombre;
+        (cliente+i)->apellido=auxApellido;
+        (cliente+i)->email=auxEmail;
+        (cliente+i)->telefono=auxTelefono;
+        (cliente+i)->fechaNacimiento=auxFecha;
+        (cliente+i)->estado=auxEstado;
+        i++;
+    }
+
+    return eCodArchivos::ExitoOperacion;
+}
+
+CodArchivos escribirArchivoCliente(std::fstream	*archi, sCliente *cliente){
+
+    if(!archi->is_open())
+        return eCodArchivos::ErrorApertura;
+
+    archi->write((char*)cliente, sizeof(sCliente));
+
+    return eCodArchivos::ExitoOperacion;
+}
